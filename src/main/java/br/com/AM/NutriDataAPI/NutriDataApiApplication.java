0f3e06ad5b67dfabeef57a0fff601a1c3924b4fe
Alison Martins <alisonmarteso@gmail.com>
2025-12-26 15:12:33 -0300

    Escopo Inicial

---
+++ src/main/java/br/com/AM/NutriDataAPI/NutriDataApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.AM.NutriDataAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriDataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriDataApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/AM/NutriDataAPI/NutriDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.AM.NutriDataAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriDataApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
